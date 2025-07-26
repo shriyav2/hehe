7b6f9746d4e568f77a3369e6b6c7553d9a7c873f
Advitiya007 <githubadvitiya@gmail.com>
2025-07-26 12:12:00 +0530

    changes made

--- backend/index.js
+++ backend/index.js
@@ -9,15 +9,20 @@ const app=express();
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 const corsopts={
-    options:[
+    origin:[
     "http://127.0.0.1:5500",
     "http://127.0.0.1:5173",
 
 ]}
 app.use(cors(corsopts))
-app.use(router,"user")
+// app.use(router,"user")
+app.use("/user", router)
 
-mongoose.connect("mongodb://127.0.0.1:27017/hack1").then(()=>{console.log("mongoose extablished")}).catch((error)=>{"there is some rror in connextion"+error.message})
+mongoose.connect("mongodb://127.0.0.1:27017/hack1")
+.then(()=>{console.log("mongoose extablished")})
+.catch((error) => {
+  console.error("There is some error in connection: " + error.message);
+});
 
 
 app.listen(PORT,()=>{
