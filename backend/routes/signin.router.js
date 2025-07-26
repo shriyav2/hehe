656f95833bf4e386a2bbc87088b92930130f740a
Advitiya007 <githubadvitiya@gmail.com>
2025-07-26 13:32:47 +0530

    changes made

--- backend/routes/signin.router.js
+++ backend/routes/signin.router.js
@@ -1,36 +1,46 @@
-import express from "express";
-import { User } from "../models/user.model.js";
-const router = express.Router();
-console.log("hi")
-// async function fxn(req,res) {
-    console.log("hi")
+// import {Router} from "express";
+// import { User } from "../models/user.model.js";
+// const router = Router();
+// console.log("hi")
+// // async function fxn(req,res) {
+//     console.log("hi")
 
-//     const { name, email, password } = req.body;
-// try{
-//       const useri = await User.findOne({ email });
-//   if (!useri) {
-//     const Useri = await User.create({ name, email, password });
-//     return res.json({ message: "User created successfully" });
-//   } else {
-//     return res.json({ message: "User already exist"});
-//   }
-// }
-// catch(err){
-//     console.log(err);
-// }
-// }
-// userrouter.post("/signin",fxn)
-
-router.post("/signup", async (req, res) => {
-  const { name,  email, password } = req.body;
-
-  await User.create({
-    name,
+// //     const { name, email, password } = req.body;
+// // try{
+// //       const useri = await User.findOne({ email });
+// //   if (!useri) {
+// //     const Useri = await User.create({ name, email, password });
+// //     return res.json({ message: "User created successfully" });
+// //   } else {
+// //     return res.json({ message: "User already exist"});
+// //   }
+// // }
+// // catch(err){
+// //     console.log(err);
+// // }
+// // }
+// // userrouter.post("/signin",fxn)
+// console.log(User)
+// router.post("/signup", async (req, res) => {
+//   const { name,  email, password } = req.body;
+// console.log(req)
+//   await User.create({
+//     name,
     
-    email,
-    password,
-  });
+//     email,
+//     password,
+//   });
 
-  return res.redirect("/");
+//   return res.json({ message: "User created successfully" })
+// });
+// export { router };
+import express from 'express';
+const router = express.Router();
+
+router.post('/signup', (req, res) => {
+    const { username, password, email } = req.body;
+    // Your signup logic here (validation, save to DB, etc.)
+    res.status(201).json({ message: 'Signup successful' });
 });
-export { router };
+
+export { router}
